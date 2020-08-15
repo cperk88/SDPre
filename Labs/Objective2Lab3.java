aff81889a56c3f1ae003c5cb31e10ff1c30b4ced
Christen Perkins <perkins.christen@gmail.com>
2020-08-14 18:33:33 -0600

    complete Objective2Lab3

---
+++ Labs/Objective2Lab3.java
@@ -0,0 +1,14 @@
+public class Objective2Lab3 {
+  public static void main(String[] args){
+    double num1, num2, num3, average;
+
+    num1 = 8;
+    num2 = 5;
+    num3 = 9;
+
+    average = (num1 + num2 + num3)/3;
+
+    System.out.println(average);
+
+  }
+}

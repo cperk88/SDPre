59ddad1fcb2a3682f916bb05f2ff4045d913249e
Christen Perkins <perkins.christen@gmail.com>
2020-08-14 18:20:12 -0600

    complete Objective2Lab2

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,13 @@
+public class Objective2Lab2 {
+  public static void main(String[] args){
+    String name = "Chris Perkins";
+    int age = 32;
+    String state = "Georgia";
+
+    System.out.println(name);
+    System.out.println(age);
+    System.out.println(state);
+
+
+  }
+}

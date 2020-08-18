7939871022e818f6e625a4b9ed4dcc0013da392f
Christen Perkins <perkins.christen@gmail.com>
2020-08-18 07:59:41 -0600

    complete Objexctive7Lab4

---
+++ Labs/Objective7Lab4.java
@@ -0,0 +1,15 @@
+public class Objective7Lab4 {
+  public static void main(String[] args) {
+    int counter = 1;
+    int total = counter;
+
+    while(counter < 20){
+      
+      counter++;
+      total += counter;
+
+    }
+
+    System.out.println(total);
+  }
+}
